59544380ef8529cee768816e46f0257816ca1b40
Luiz Matos <luiz.matos@amedigital.com>
2021-01-23 00:23:16 -0300

    feat:
    - adds preprocessing scripts for reddit dataset;
    - fixes top 10 topic words for coherence calculation;
    - adds etm_trainer script for reddit use case;
    - adds pipfile;
    - adds constants and utils scripts for reddit use case;
    - keeps every word on word2vec training

---
+++ training/constants.py
@@ -0,0 +1,2 @@
+CSV_RESULTS_FOLDER = "models_training/results/"
+MODELS_FOLDER = "models_training/models/"
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import torch 
 import numpy as np
+from gensim.models import CoherenceModel
 
 def get_topic_diversity(beta, topk):
     num_topics = beta.shape[0]
@@ -10,6 +11,7 @@ def get_topic_diversity(beta, topk):
     n_unique = len(np.unique(list_w))
     TD = n_unique / (topk * num_topics)
     print('Topic diveristy is: {}'.format(TD))
+    return TD
 
 def get_document_frequency(data, wi, wj=None):
     if wj is None:
@@ -37,30 +39,30 @@ def get_document_frequency(data, wi, wj=None):
                 D_wi_wj += 1
     return D_wj, D_wi_wj 
 
-def get_topic_coherence(beta, data, vocab):
+def get_topic_coherence(beta, data, vocab, top_n = 10):
     D = len(data) ## number of docs...data is list of documents
     print('D: ', D)
     TC = []
     num_topics = len(beta)
     for k in range(num_topics):
         print('k: {}/{}'.format(k, num_topics))
-        top_10 = list(beta[k].argsort()[-11:][::-1])
-        top_words = [vocab[a] for a in top_10]
+        beta_top_n = list(beta[k].argsort()[-top_n:][::-1])
+        top_words = [vocab[a] for a in beta_top_n]
         TC_k = 0
         counter = 0
-        for i, word in enumerate(top_10):
+        for i, word in enumerate(beta_top_n):
             # get D(w_i)
             D_wi = get_document_frequency(data, word)
             j = i + 1
             tmp = 0
-            while j < len(top_10) and j > i:
+            while j < len(beta_top_n) and j > i:
                 # get D(w_j) and D(w_i, w_j)
-                D_wj, D_wi_wj = get_document_frequency(data, word, top_10[j])
+                D_wj, D_wi_wj = get_document_frequency(data, word, beta_top_n[j])
                 # get f(w_i, w_j)
                 if D_wi_wj == 0:
                     f_wi_wj = -1
                 else:
-                    f_wi_wj = -1 + ( np.log(D_wi) + np.log(D_wj)  - 2.0 * np.log(D) ) / ( np.log(D_wi_wj) - np.log(D) )
+                    f_wi_wj = -1 + ( np.log(D_wi) + np.log(D_wj) - 2.0 * np.log(D) ) / ( np.log(D_wi_wj) - np.log(D) )
                 # update tmp: 
                 tmp += f_wi_wj
                 j += 1
@@ -72,6 +74,33 @@ def get_topic_coherence(beta, data, vocab):
     print('num topics: ', len(TC))
     TC = np.mean(TC) / counter
     print('Topic coherence is: {}'.format(TC))
+    return TC
+
+
+def get_gensim_coherence(topics, documents, dictionary, coherence):
+    """Calculates topic coherence using gensim's coherence pipeline.
+
+    Parameters:
+
+    topics (list of str list): topic words for each topic
+    
+    documents (list of str): set of documents
+
+    dictionary (gensim.corpora.Dictionary): gensim dicionary of words from dataset
+
+    coherence (str): coherence type. Can be 'c_v', 'u_mass', 'c_uci' or 'c_npmi'
+
+    Returns:
+
+    float: coherence score
+    """
+    return CoherenceModel(
+        topics=topics, 
+        texts=documents, 
+        dictionary=dictionary, 
+        coherence=coherence
+    ).get_coherence()
+
 
 def nearest_neighbors(word, embeddings, vocab):
     vectors = embeddings.data.cpu().numpy() 
