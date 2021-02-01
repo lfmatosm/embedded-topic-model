916201006183054ecfe8bfefa44205a32e4e3321
Luiz Matos <luiz.matos@amedigital.com>
2021-01-31 22:49:42 -0300

    chore:
    - adds example script to create a test_resources obj;
    - moves original article's dataset preprocessing files to script/datasets folder

---
+++ embedded_topic_model/scripts/misc/create_test_resources.py
@@ -0,0 +1,25 @@
+from embedded_topic_model.utils import embedding, preprocessing
+import joblib
+import os
+
+
+sentences = [
+    "Peanut butter and jelly caused the elderly lady to think about her past.",
+    "Toddlers feeding raccoons surprised even the seasoned park ranger.",
+    "You realize you're not alone as you sit in your bedroom massaging your calves after a long day of playing tug-of-war with Grandpa Joe in the hospital.",
+    "She wondered what his eyes were saying beneath his mirrored sunglasses.",
+    "He was disappointed when he found the beach to be so sandy and the sun so sunny.",
+    "Flesh-colored yoga pants were far worse than even he feared.",
+    "The wake behind the boat told of the past while the open sea for told life in the unknown future.",
+    "Improve your goldfish's physical fitness by getting him a bicycle.",
+    "Harrold felt confident that nobody would ever suspect his spy pigeon.",
+    "Nudist colonies shun fig-leaf couture.",
+]
+vocabulary, train_dataset, test_dataset = preprocessing.create_etm_datasets(sentences)
+
+embeddings = embedding.create_word2vec_embedding_from_dataset(sentences)
+
+os.makedirs(os.path.dirname('./train_resources.test'), exist_ok=True)
+joblib.dump((vocabulary, embeddings, train_dataset, test_dataset), './train_resources.test', compress=8)
+
+print('the end')
