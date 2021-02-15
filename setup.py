abf5a0fda86c87b1e244e16d7ee75f61e29be594
Luiz Matos <luiz.matos@amedigital.com>
2021-02-15 18:57:17 -0300

    chore: bump version

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     test_suite='tests',
     tests_require=dev_requirements,
     url='https://github.com/lffloyd/embedded-topic-model',
-    version='0.1.1',
+    version='1.0.0',
     zip_safe=False,
 )
